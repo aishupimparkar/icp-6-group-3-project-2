04f128e899a2debb6a2f2e70e785e61640a3826c
harshaal06 <harshalaglawe06@gmail.com>
2023-12-22 14:48:14 +0530

    color change

--- js/book_now.js
+++ js/book_now.js
@@ -2,13 +2,13 @@ function submitForm() {
     alert('Booking successful!');
 }
 
-const quantity = document.getElementById("quantity");
+let quantity = document.getElementById("quantity");
+function quantityDecrease() {
+    alert('hello');
+    let quantityDec = parseInt(quantity.innerText);
+    quantity.innerText = --quantityDec;
+}
 function quantityIncrease() {
     let quantityInc = parseInt(quantity.innerText);
     quantity.innerText = ++quantityInc;
-}
-function quantityDecrease() {
-    alert('hello');
-    let quantityInc = parseInt(quantity.innerText);
-    quantity.innerText = --quantityInc;
 }
